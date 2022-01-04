fb183f74af99584a88f8de31d2129cc64de1b7ef
한재원 <hanjo8813@gmail.com>
2022-01-05 06:41:02 +0900

    재원: W1D2 - 실패 (#3)

---
+++ 1.baekjoon/hanjo/example/Main.java
@@ -0,0 +1,20 @@
+package example;
+
+import java.util.*;
+
+public class Main {
+
+    public static int solution(int a, int b){
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(solution(a, b));
+    }
+    
+}
