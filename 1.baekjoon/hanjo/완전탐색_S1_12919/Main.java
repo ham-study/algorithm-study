8f30cb4184baeed21924dd236d61eed098c45c30
한재원 <hanjo8813@gmail.com>
2022-01-06 04:36:13 +0900

    입력방식 변경

--- 1.baekjoon/hanjo/완전탐색_S1_12919/Main.java
+++ 1.baekjoon/hanjo/완전탐색_S1_12919/Main.java
@@ -1,6 +1,6 @@
 package 완전탐색_S1_12919;
 
-import java.util.*;
+import java.io.*;
 
 public class Main {
 
@@ -48,11 +48,11 @@ public class Main {
         return answer;
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        String s = sc.nextLine();
-        String t = sc.nextLine();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String s = br.readLine();
+        String t = br.readLine();
 
         System.out.println(solution(s, t));
     }
@@ -61,9 +61,12 @@ public class Main {
 
 /**
  * 링크 : https://www.acmicpc.net/problem/12919 
- * 성공여부 : 재귀까지는 생각했으나 두 조건을 동시 실행해야 하는 경우에서 뇌정지가 와서 결국 구글링 함
+ * 성공여부 : X (재귀까지는 생각했으나 두 조건을 동시 실행해야 하는 경우에서 뇌정지가 와서 결국 구글링 함)
  * 풀이시간 : 1h +
+ * 
  * 시간복잡도 : O(n) (재귀가 n번 돌때)
+ * 메모리 : 17724 KB
+ * 소요 시간 : 212 ms
  * 
  * StringBuilder - reverse : 문자열을 거꾸로 돌리기
  * StringBuilder - deleteCharAt : 문자열에서 인덱스에 해당하는 값을 삭제
