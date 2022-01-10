ee11e2e2da44d87e600c91a994c9e9626a8776c3
지훈 <goharrm@ajou.ac.kr>
2022-01-10 11:14:36 +0900

    sol: boj 11726 (#29)
    * Create boj_1260.java
    
    * sol: boj 2504 (fail)
    
    * Update boj_2504.java
    
    * sol: boj 11726
    
    * Update boj_11726.java

---
+++ 1.baekjoon/jihun/boj_11726.java
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+ 문제출처 : https://www.acmicpc.net/problem/11726
+ 풀이시간 : 30분
+ 시간복잡도 : O(n) (n: 입력 길이) 입력 길이만큼 반복문을 돌기 때문(bottom-up)
+ 공간복잡도 : O(n)
+
+ *******************************************************************************/
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        var br = new BufferedReader(new InputStreamReader(System.in));
+        int size = Integer.parseInt(br.readLine());
+        br.close();
+
+        int[] dp = new int[1001];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        if (size == 1 || size == 2) {
+            System.out.println(dp[size]);
+            return;
+        }
+
+        for (int i = 3; i <= size; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+        }
+
+        System.out.println(dp[size]);
+    }
+
+}
