18b4e39a1f6ef340ecc4446fc7123b98221826db
한재원 <hanjo8813@gmail.com>
2022-02-07 23:33:27 +0900

    파일정리

---
+++ 1.baekjoon/hanjo/boj/정렬_G5_선긋기/Main.java
@@ -0,0 +1,30 @@
+package 정렬_G5_선긋기;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+    }
+}
+
+/**
+ * ================================================================================
+ * 링크 : https://www.acmicpc.net/problem/2170
+ * 성공여부 : 
+ * 풀이시간 : 
+ * 
+ * 시간복잡도 : 
+ * 메모리 : KB
+ * 소요 시간 : ms
+ * ================================================================================
+ * 
+ * 
+ */
