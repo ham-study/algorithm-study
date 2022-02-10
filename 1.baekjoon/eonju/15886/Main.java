752ab64135323fba74f8fa14996a0bd029662944
GetMapping <eonju.dev@gmail.com>
2022-02-10 10:25:55 +0900

    solve: Boj 15886 #107

---
+++ 1.baekjoon/eonju/15886/Main.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
+        String[] road = bufferedReader.readLine().split("");
+
+        int answer = 0;
+        int count = 0;
+        String flag = road[0];
+        for (int i = 1; i < n; i++) {
+            if (!flag.equals(road[i])) {
+                if (count == 0) {
+                    count++;
+                    flag = road[i];
+                } else {
+                    answer += 1;
+                    count = 0;
+                    flag = road[i];
+                }
+            }
+        }
+
+        System.out.println(answer + 1);
+    }
+}
