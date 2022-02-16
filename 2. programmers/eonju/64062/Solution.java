3b26529b45af22cdf83f96065eb32a41eca22036
GetMapping <eonju.dev@gmail.com>
2022-02-16 11:22:04 +0900

    solve : BOJ 64062 #130

--- 2. programmers/eonju/64062/Solution.java
+++ 2. programmers/eonju/64062/Solution.java
@@ -1,9 +1,5 @@
 package programmers;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Stack;
-
 public class PMS_64062 {
 
     public static void main(String[] args) {
@@ -12,34 +8,39 @@ public class PMS_64062 {
 
     public static int solution(int[] stones, int k) {
         int answer = 0;
-        int[] compareArray = Arrays.stream(stones)
-            .distinct()
-            .sorted()
-            .toArray();
+        int min = 1;
+        int max = 200000000;
 
-        for (int i = 0; i < compareArray.length; i++) {
-            if (countContinuousZero(stones, compareArray[i]) == k) {
-                answer = compareArray[i];
-                break;
+        while (min <= max) {
+            int mid = (min + max) / 2;
+
+            if (canAcrossRiver(stones, k, mid)) {
+                min = mid + 1;
+                answer = Math.max(answer, mid);
+            } else {
+                max = mid - 1;
             }
         }
 
         return answer;
     }
 
-    public static int countContinuousZero(int[] stones, int target) {
-        ArrayList<Integer> counts = new ArrayList<>();
-        Stack<Integer> stack = new Stack<>();
+    public static boolean canAcrossRiver(int[] stones, int k, int friends) {
+        int skip = 0;
 
-        for (int i = 0; i < stones.length; i++) {
-            if (stones[i] <= target) {
-                stack.add(0);
+        for (int stone : stones) {
+            if (stone - friends < 0) {
+                skip++;
             } else {
-                counts.add(stack.size());
-                stack.clear();
+                skip = 0;
+            }
+
+            if (skip == k) {
+                return false;
             }
         }
 
-        return counts.stream().mapToInt(i -> i).max().getAsInt();
+        return true;
     }
+
 }
